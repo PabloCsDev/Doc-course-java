8d9850936e586a63aa06bd923be132e9733bcb18
PabloCsDev <devpablocarvalho@gmail.com>
2023-10-21 21:06:20 -0300

    update

--- POO/a64_Application/Program.java
+++ POO/a64_Application/Program.java
@@ -1,30 +1,38 @@
 package a64_Application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import a64_Entities.Triangle;
+
 public class Program {
 	
 	public static void main (String [] args) {
-		public static void main(String[] args) {
 			Locale.setDefault(Locale.US);
 			Scanner sc = new Scanner(System.in);
-			double xA, xB, xC, yA, yB, yC;
+			
+			
+			Triangle x, y;
+			x = new Triangle();
+			y = new Triangle();
 
 			System.out.println("Digite o valor dos lados A, B e C do triangulo X: ");
-			xA = sc.nextDouble();
-			xB = sc.nextDouble();
-			xC = sc.nextDouble();
+			x.a = sc.nextDouble();
+			x.b = sc.nextDouble();
+			x.c = sc.nextDouble();
 			System.out.println();
 
 			System.out.println("Digite o valor dos lados A, B e C do triangulo y: ");
-			yA = sc.nextDouble();
-			yB = sc.nextDouble();
-			yC = sc.nextDouble();
+			y.a = sc.nextDouble();
+			y.b = sc.nextDouble();
+			y.c = sc.nextDouble();
 			System.out.println();
 
-			double p = (xA + xB + xC) / 2.0;
-			double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+			double p = (x.a + x.b + x.c) / 2.0;
+			double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 
-			p = (yA + yB + yC) / 2.0;
-			double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+			p = (y.a + y.b + y.c) / 2.0;
+			double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 
 			System.out.printf("A area do triangulo X é: %.4f%n", areaX);
 			System.out.printf("A area do triangulo Y é: %.4f%n", areaY);
@@ -41,4 +49,4 @@ public class Program {
 
 	}
 
-}
+
--- POO/a64_Entities/Triangle.java
+++ POO/a64_Entities/Triangle.java
@@ -2,4 +2,7 @@ package a64_Entities;
 
 public class Triangle {
 
+	public double a,b,c;
+	
+
 }

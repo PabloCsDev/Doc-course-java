0e7e78d89a47960dcd46caed157931f709206256
PabloCsDev <devpablocarvalho@gmail.com>
2023-10-23 20:51:13 -0300

    update

---
+++ POO/a66_Application/Program.java
@@ -0,0 +1,28 @@
+package a66_Application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import a66_Entities.Product;
+
+public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.println("Name: ");
+		product.name = sc.nextLine();
+		System.out.println("Price: ");
+		product.price = sc.nextDouble();
+		System.out.println("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+
+		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+
+		sc.close();
+
+	}
+
+}

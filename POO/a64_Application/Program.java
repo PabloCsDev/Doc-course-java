ee798e34698ea0614b4eaecb12d362db6fa83fb7
PabloCsDev <devpablocarvalho@gmail.com>
2023-10-22 21:13:45 -0300

    update

--- POO/a64_Application/Program.java
+++ POO/a64_Application/Program.java
@@ -6,47 +6,40 @@ import java.util.Scanner;
 import a64_Entities.Triangle;
 
 public class Program {
-	
-	public static void main (String [] args) {
-			Locale.setDefault(Locale.US);
-			Scanner sc = new Scanner(System.in);
-			
-			
-			Triangle x, y;
-			x = new Triangle();
-			y = new Triangle();
 
-			System.out.println("Digite o valor dos lados A, B e C do triangulo X: ");
-			x.a = sc.nextDouble();
-			x.b = sc.nextDouble();
-			x.c = sc.nextDouble();
-			System.out.println();
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
-			System.out.println("Digite o valor dos lados A, B e C do triangulo y: ");
-			y.a = sc.nextDouble();
-			y.b = sc.nextDouble();
-			y.c = sc.nextDouble();
-			System.out.println();
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
 
-			double p = (x.a + x.b + x.c) / 2.0;
-			double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+		System.out.println("Digite o valor dos lados A, B e C do triangulo X: ");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		System.out.println();
 
-			p = (y.a + y.b + y.c) / 2.0;
-			double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		System.out.println("Digite o valor dos lados A, B e C do triangulo y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		System.out.println();
 
-			System.out.printf("A area do triangulo X é: %.4f%n", areaX);
-			System.out.printf("A area do triangulo Y é: %.4f%n", areaY);
+		double areaX = x.area();
 
-			if (areaX > areaY) {
-				System.out.println("Maior area: X");
-			} else {
-				System.out.println("Maior area: Y");
-			}
+		double areaY = y.area();
+		System.out.printf("A area do triangulo X é: %.4f%n", areaX);
+		System.out.printf("A area do triangulo Y é: %.4f%n", areaY);
 
-			sc.close();
+		if (areaX > areaY) {
+			System.out.println("Maior area: X");
+		} else {
+			System.out.println("Maior area: Y");
 		}
 
-
+		sc.close();
 	}
 
-
+}
--- POO/a64_Entities/Triangle.java
+++ POO/a64_Entities/Triangle.java
@@ -4,5 +4,14 @@ public class Triangle {
 
 	public double a,b,c;
 	
+	
+	public double area() {
+		double p = (a + b + c) / 2.0;
+		 return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
+	
+	
+	
 
 }
+ 

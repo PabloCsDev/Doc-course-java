bebccd1da29fd61f8e24d346eaafa1410aab6055
PabloCsDev <devpablocarvalho@gmail.com>
2023-10-30 19:14:11 -0300

    update

--- POO/a81_Application/Program.java
+++ POO/a81_Application/Program.java
@@ -6,15 +6,7 @@ import a81_Entities.P01_Bank;
 
 public class Program {
 	public static void main (String [] args ) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner (System.in);
 		
-		P01_Bank bank = new P01_Bank();
-
-		
-		
-		
-		sc.close();
 	}
 
 }

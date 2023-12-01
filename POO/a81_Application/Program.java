68f25d91d405984facc6124d197474cf751a222e
PabloCsDev <devpablocarvalho@gmail.com>
2023-11-30 21:29:35 -0300

    update

--- POO/a81_Application/Program.java
+++ POO/a81_Application/Program.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import a81_Entities.Account;
 
 public class Program {
-	public static void main(String[] args) {
+	public static void main(String[]  args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		Account account;
